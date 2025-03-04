c1df5eccea1f73335aa82700ca9a5fc65e7cf40d
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-03-04 21:08:41 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import CatatanList from "./components/CatatanList";
+import AddCatatan from "./components/AddCatatan";
+import EditCatatan from "./components/EditCatatan";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CatatanList/>}/>
+        <Route path="add" element={<AddCatatan/>}/>
+        <Route path="edit/:id" element={<EditCatatan/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
