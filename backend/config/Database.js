fc45557306a2512473b65da1e4c05480d0fe3b72
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-03-10 20:09:50 +0700

    ngubah ke awal
    sorry banget mas, mau ngubah ke yang awal soalnya sempet keubah utk tugas migrasi database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
 const db = new Sequelize("123220134_Catatan", "root", "", {
-  host: "35.223.100.248",
+  host: "localhost",
   dialect: "mysql",
 });
 
